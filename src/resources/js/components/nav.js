c2c1ce6e413edd51444ada320f1afb153bfc931c
Scott A Eckenthal <scott.eckenthal@gmail.com>
2016-05-21 09:37:34 -0400

    Packs nav into separate file

---
+++ src/resources/js/components/nav.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var $ = require('jquery');
+
+function Nav(ops) {
+	ops = ops || {};
+	this.visible = false;
+
+	if (!ops.$scoper || !ops.$scoper instanceof $ || ops.$scoper.length < 1) {
+		throw Error('Initialization of component nav requires arg `ops` to have non-empty config field `$scoper` whose value is a non-empty jQuery element, but received '+ ops.$scoper);
+	}
+	if (!ops.$header || !ops.$header instanceof $ || ops.$header.length < 1) {
+		throw Error('Initialization of component nav requires arg `ops` to have non-empty config field `$header` whose value is a non-empty jQuery element, but received '+ ops.$header);
+	}
+	if (!ops.$trigger || !ops.$trigger instanceof $ || ops.$trigger.length < 1) {
+		throw Error('Initialization of component nav requires arg `ops` to have non-empty config field `$trigger` whose value is a non-empty jQuery element, but received '+ ops.$trigger);
+	}
+
+	this.$scoper = ops.$scoper;
+	this.$header = ops.$header;
+	this.$trigger = ops.$trigger;
+}
+
+Nav.prototype.initialize = function() {
+	var self = this;
+	this.$trigger.on('click', function() {
+		self.toggle();
+	});
+	return this;
+}
+
+Nav.prototype.toggle = function() {
+	if (this.visible) {
+		this.hide();
+	} else {
+		this.show();
+	}
+	return this;
+};
+
+Nav.prototype.show = function() {
+
+	var headerHeight = this.$header.outerHeight;
+	this.$header.css({ 'top': 0 });
+	this.$scoper.css({ 'padding-top': 123 })
+
+	this.visible = true;
+	return this;
+};
+
+Nav.prototype.hide = function() {
+	this.$header.css({ 'top': -75 });
+	this.$scoper.css({ 'padding-top': 48 });
+
+	this.visible = false;
+	return this;
+};
+
+module.exports = Nav;
