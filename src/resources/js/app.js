db421a867ad68b21c85fc730b8395303278c3d0a
Scott A Eckenthal <scott.eckenthal@gmail.com>
2016-05-21 21:00:31 -0400

    Converts nav implementation to pure-css dependent on hacky nav-height measurement

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,13 +2,8 @@
 
 var $ = require('jquery');
 
-var Nav = require('./components/nav');
-
 $(function() {
-	var nav = new Nav({
-		$scoper: $('.page-scoper'),
-		$header: $('.page-header'),
-		$trigger: $('.trigger')
-	}).initialize();
+	$('.trigger').on('click', function() {
+		$('body').toggleClass('nav-active');
+	});
 });
-
