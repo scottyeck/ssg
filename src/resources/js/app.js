e02c50a62f2b3c132cf1453479acedcb1c1ee919
Scott A Eckenthal <scott.eckenthal@gmail.com>
2016-05-21 21:31:45 -0400

    Disables padding transitions on page resize

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,9 +1,25 @@
 'use strict';
 
-var $ = require('jquery');
+var $ = require('jquery'),
+	debounce = require('lodash.debounce'),
+	throttle = require('lodash.throttle');
+
+var _ = {
+	debounce: debounce,
+	throttle: throttle	
+};
 
 $(function() {
+
 	$('.trigger').on('click', function() {
 		$('body').toggleClass('nav-active');
 	});
+
+	$(window).on('resize', _.throttle(function() {
+		$('body').addClass('disable-transition');
+	}, 250));
+
+	$(window).on('resize', _.debounce(function() {
+		$('body').removeClass('disable-transition');
+	}, 250));
 });
