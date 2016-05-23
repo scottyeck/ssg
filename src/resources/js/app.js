83d5eff5fdc5e60e534fddc0655c45123a9d4ceb
Scott A Eckenthal <scott.eckenthal@gmail.com>
2016-05-23 19:57:09 -0400

    Keeps trigger visible for several seconds after nav has closed

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -11,8 +11,55 @@ var _ = {
 
 $(function() {
 
+	var trigger,
+		menu;
+
+	function Menu(ops) {
+		this.visible = false;
+	}
+
+	Menu.prototype.hide = function() {
+		var self = this;
+		$('body').removeClass('nav-active');
+		this.visible = false;
+		setTimeout(function() {
+			trigger.hide();
+		}, 2000);
+	};
+
+	Menu.prototype.show = function() {
+		$('body').addClass('nav-active');
+		trigger.show();
+		this.visible = true;
+	};
+
+	Menu.prototype.toggle = function() {
+		if (this.visible) {
+			this.hide();
+		} else {
+			this.show();
+		}
+	};
+
+	function Trigger() {
+		this.visible = true;
+	}
+
+	Trigger.prototype.hide = function() {
+		$('body').removeClass('show-trigger');
+		this.visible = false;
+	};
+
+	Trigger.prototype.show = function() {
+		$('body').addClass('show-trigger');
+		this.visible = true;
+	};
+
+	trigger = new Trigger();
+	menu = new Menu();
+
 	$('.trigger').on('click', function() {
-		$('body').toggleClass('nav-active');
+		menu.toggle();
 	});
 
 	$(window).on('resize', _.throttle(function() {
@@ -24,12 +71,14 @@ $(function() {
 	}, 250));
 
 	$('.page-scoper').on('scroll', _.throttle(function() {
-		$('body').addClass('show-trigger');
+		trigger.show();
 	}, 250));
 
 	$('.page-scoper').on('scroll', _.debounce(function() {
 		setTimeout(function() {
-			$('body').removeClass('show-trigger');
-		}, 3000);
+			if (!menu.visible) {
+				trigger.hide();
+			}
+		}, 2000);
 	}, 250));
 });
